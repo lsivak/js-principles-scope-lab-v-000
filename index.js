a2fe5a8478476ae713331d426f3a7afef799d3e6
Lara Sanders <larasanders@gmail.com>
2018-05-17 23:55:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 customerName = "bob"
 
 function upperCaseCustomerName(customerName) {
-  var customerName = "BOB"
+  customerName = customerName.toUpperCase
 return customerName;
 }
 // console.log('Return value of upperCaseCustomerName(customerName)')
